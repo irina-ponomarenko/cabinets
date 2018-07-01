adde9ae5c5652de2a9f4d61eb89b07f24a3b7322
Zhe1ka <zheka.dzecina@gmail.com>
2018-07-01 10:27:28 +0300

    init

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@ $(document).ready(function () {
     $('.jcarousel').jcarouselSwipe({
         perSwipe: 3 // by default 1
     });
-    
+
+    $('.nav-bar-left').drawer({
+        align: 'left',
+        top: '150px'
+    });
 });
